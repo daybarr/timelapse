cf682a0ead28c7096b4dc7092ccf1b64fd7d6a08
Day Barr <github.com@daybarr.com>
2015-03-25 00:03:21 +0000

    Add ability to specify src dir

--- server/download.py
+++ server/download.py
@@ -14,6 +14,7 @@ LOG_FORMAT = '%(asctime)-15s %(name)-10s %(levelname)-7s %(message)s'
 # once helps avoid lockups/hangs on the android side.
 MAX_PER_CONN = 10
 
+DEFAULT_SRC_DIR = os.path.expanduser("/mnt/sdcard/timelapse")
 DEFAULT_DEST_DIR = os.path.expanduser("~/timelapse")
 
 LOG_FILE_NAME = 'battery.txt'
@@ -27,6 +28,8 @@ def parse_args(argv):
     parser.add_argument('password', help="Password for ssh server")
     parser.add_argument('--dest-dir', default=DEFAULT_DEST_DIR,
                         help="Directory to download photos to")
+    parser.add_argument('--src-dir', default=DEFAULT_SRC_DIR,
+                        help="Directory to download photos from")
     args = parser.parse_args(argv[1:])
     if not os.path.isdir(args.dest_dir):
         parser.error("--dest-dir {} must exist".format(args.dest_dir))
@@ -37,7 +40,9 @@ def download_some(args):
     logger.info("Connecting")
     with pysftp.Connection(args.ip, port=args.port,
                            username=args.username,
-                           password=args.password) as sftp:
+                           password=args.password) as sftp, \
+        sftp.cd(args.src_dir):
+
         logger.info("Listing files")
         file_names = sftp.listdir()
         logger.info("Found %d files", len(file_names))
